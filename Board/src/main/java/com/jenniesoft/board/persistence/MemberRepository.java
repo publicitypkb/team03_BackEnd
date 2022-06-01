77a9b34b1befa5f532b9fb49c63233a59e72a469
publicitypkb <106524603+publicitypkb@users.noreply.github.com>
2022-06-01 11:37:06 +0900

    Delete Board directory

--- Board/src/main/java/com/jenniesoft/board/persistence/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jenniesoft.board.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jenniesoft.board.model.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String>{
-
-}
