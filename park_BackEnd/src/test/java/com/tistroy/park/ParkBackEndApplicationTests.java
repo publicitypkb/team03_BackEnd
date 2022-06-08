74987ebba62d830e4d044a5a754ce7e498ffe804
pkg10 <pkg10@DESKTOP-E2CTGM3>
2022-06-08 17:13:10 +0900

    entry point

---
+++ park_BackEnd/src/test/java/com/tistroy/park/ParkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistroy.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
